4794fa3cc9fbf639fd6d8ce715f2719b025248ef
Gulrez Farooqui <gulfa@192.168.0.8>
2019-11-07 00:06:32 +0530

    sdfs

---
+++ src/test/java/com/gul/test/Student.java
@@ -0,0 +1,25 @@
+package com.gul.test;
+
+public class Student {
+	private String name;
+	private String maths;
+	private String science;
+	private String english;
+
+	public Student() {
+	}
+
+	public Student(String name, String maths, String science, String english) {
+		this.name = name;
+		this.maths = maths;
+		this.science = science;
+		this.english = english;
+	}
+
+	// getters and setter..
+
+	@Override
+	public String toString() {
+		return name + ": Maths " + maths + " Science " + science + " English " + english;
+	}
+}
